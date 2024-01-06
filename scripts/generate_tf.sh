389b28bff08d472f3caf3d98e107c7229212b407
Alberto Mordonini <albmord@gmail.com>
2024-01-06 21:54:27 +0100

    sync

--- scripts/generate_tf.sh
+++ scripts/generate_tf.sh
@@ -1,12 +1,20 @@
 MODEL_PATH=$1
 
 
+#python src/generate.py \
+#    --data_folder data/ \
+#    --model_folder mix \
+#    --pretrained_model_name_or_path $MODEL_PATH \
+#    --tokenizer_name_or_path meta-llama/Llama-2-7b-hf \
+#    --num_return_sequences 40 \
+#    --do_sample True \
+#    --temperature 0.8 \
+#    --prompt_template_path prompts/llama_2.txt
+
+
 python src/generate.py \
     --data_folder data/ \
     --model_folder mix \
     --pretrained_model_name_or_path $MODEL_PATH \
     --tokenizer_name_or_path meta-llama/Llama-2-7b-hf \
-    --num_return_sequences 40 \
-    --do_sample True \
-    --temperature 0.8 \
     --prompt_template_path prompts/llama_2.txt
