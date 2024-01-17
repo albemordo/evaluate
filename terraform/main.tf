0bb08c44a28b0d95fac22c2ef48f8f73f2f77b5e
mralby <albmord@gmail.com>
2024-01-17 21:21:22 +0100

    sync

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,26 @@
+/*
+PROMPT:
+Provide terraform hcl code to create an aws provider and an aws dynamodb table. 
+The region of the provider block must be set to "eu-west-3".
+The dynamodb table block should be named 'my_table'
+  and must contain a single column (attribute) named "Attribute_1" of type string ("S"), 
+  this attribute is also the hash key. 
+Also set 'read_capacity' and 'write_capacity' to 1.
+*/
+
+####
+
+
 provider "aws" {
   region = "eu-west-3"
 }
 
 resource "aws_dynamodb_table" "my_table" {
   name = "my_table"
-  read_capacity = 1
-  write_capacity = 1
-  hash_key = "Attribute_1"
   attribute {
     name = "Attribute_1"
     type = "S"
   }
+  read_capacity = 1
+  write_capacity = 1
 }
